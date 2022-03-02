944e163b4e72aec218b52071b8694b523e133e32
Brian Colclough <brianmcolclough@gmail.com>
2022-03-02 18:35:56 -0500

    up to page 30

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -3,7 +3,34 @@ from test_framework import generic_test
 
 def parity(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    # Optimal
+    print(bin(x)) 
+    x ^= x >> 32
+    print(bin(x))
+    x ^= x >> 16
+    x ^= x >> 8
+    print(x)
+    x ^= x >> 4
+    x ^= x >> 2
+    print(x)
+    x ^= x >> 1
+    return x & 0x1
+
+
+    # one optimization to brute force
+    # result = 0
+    # while x:
+    #     result ^= 1
+    #     x &= x-1 # Drops the lowest set bit of x
+    # return result
+    
+
+    # Brute Force solution
+    # one_bits = 0
+    # while x:
+    #     one_bits ^= x & 1
+    #     x>>=1
+    # return one_bits
 
 
 if __name__ == '__main__':
--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -3,7 +3,15 @@ from test_framework import generic_test
 
 def swap_bits(x, i, j):
     # TODO - you fill in here.
-    return 0
+    # Extract the i-th and j-th bits, and see if they differ
+    if (x>>i) & 1 != (x>>j)&1: 
+        # i-th and j-th bits differ. We will swap them by flipping their values.
+        #select the bits to flip with bit_mask. Since x^1 = 0 whtn x = 1 and 1 when x = 0
+        # we can perform the flip XOR
+        bit_mask = (1<<i) | (1<<j) #this combines the two binarys with 1's in the
+        # position of both i and j and zeros everywhere else
+        x ^= bit_mask
+    return x
 
 
 if __name__ == '__main__':
